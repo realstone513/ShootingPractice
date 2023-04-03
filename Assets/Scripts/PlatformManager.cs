15ffd9e4be7c63b0ff3553ced40d31c6e15ed35e
realstone513 <ting6247@nate.com>
2023-04-04 01:51:43 +0900

    플랫폼 매니저 구현 (백그라운드 종스크롤)

---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public GameObject[] backgrounds;
+    public float backgroundSize;
+    public float platformSpeed;
+
+    private void Update()
+    {
+        int length = backgrounds.Length;
+        for (int i = 0; i < length; i++)
+        {
+            backgrounds[i].transform.Translate(platformSpeed * Time.deltaTime * Vector2.down);
+            if (backgrounds[i].transform.position.y < -backgroundSize)
+                backgrounds[i].transform.Translate(2 * backgroundSize * Vector3.up);
+        }
+    }
+}
