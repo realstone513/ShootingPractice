f18c751dbd6be662903146eba97b096f9a71f5e2
realstone513 <ting6247@nate.com>
2023-04-05 12:08:25 +0900

    적ai 수정

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,13 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
     public Vector2 direction = Vector2.down;
     public float speed = 3f;
+    public List<GameObject> weapons;
 
     private void Update()
     {
         if (gameObject.CompareTag("Enemy"))
             gameObject.transform.Translate(speed * Time.deltaTime * direction);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Border") && collision.name.Equals("Bottom"))
+        {
+            Debug.Log(collision.name);
+            GameManager.Instance.DestroyAircraft(gameObject);
+        }
     }
 }
